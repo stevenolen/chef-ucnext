d86bbb03eb5ae2d92c1ea822e8097ce6b2914ebe
Joshua Selsky <jselsky@oit.ucla.edu>
2016-05-04 16:13:48 -0700

    Add policy file to help fix ImageTragic vuln.

--- libraries/provider_ucnext.rb
+++ libraries/provider_ucnext.rb
@@ -37,6 +37,15 @@ class Chef
           variables(config: new_resource)
         end
 
+        template "/etc/ImageMagick/policy.xml" do
+          owner 'root'
+          group 'root'
+          mode '0755'
+          source 'image-magick-policy.xml.erb'
+          cookbook 'ucnext'
+          variables(config: new_resource)
+        end
+
         # add shared dirs for chef deploy
         directory "#{new_resource.deploy_path}/shared" do
           recursive true
